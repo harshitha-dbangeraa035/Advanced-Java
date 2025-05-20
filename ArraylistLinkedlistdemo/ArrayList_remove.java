3cc7adf7e35df15fe0c8ed401a48c700b7a24fc5
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 20:16:17 +0530

    Update ArrayList_remove.java

--- ArraylistLinkedlistdemo/ArrayList_remove.java
+++ ArraylistLinkedlistdemo/ArrayList_remove.java
@@ -7,8 +7,6 @@ import java.util.List;
 public class lab3 {
     public static void main(String args[]) {
         List<String> colors = new ArrayList<>();
-        
-        // Adding colors to the list
         colors.add("Blue");
         colors.add("Green");
         colors.add("Red");
@@ -17,14 +15,9 @@ public class lab3 {
         colors.add("Maroon"); 
         colors.add("Grey");   
         colors.add("Pink");   
-
         System.out.println("Initial list: " + colors);
-
-        
         colors.remove("Green");
         System.out.println("After removing Green: " + colors);
-
-        
         if (colors.contains("Blue")) {
             colors.remove("Blue");
             System.out.println("After removing Blue: " + colors);
