ca602688d03a88e18b542b46b708db96af473178
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 20:01:33 +0530

    Create Arraylist_delete.java

---
+++ ArraylistLinkedlistdemo/Arraylist_delete.java
@@ -0,0 +1,22 @@
+/*Write a java program for getting different colors through ArrayList interface and delete nth element from the ArrayList object by using remove by index*/
+package adjlab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lab6 {
+	public static void main(String args[]){
+		List<String> a=new ArrayList<>();
+		a.add("Blue");
+		a.add("Green");
+		a.add("Red");
+		a.add("Navy");
+		a.add("Brown");
+		a.add("Maroon");
+		a.add("Grey");
+		System.out.println("Original list: " +a);
+		int n=a.size();
+		a.remove(n-1);
+		System.out.println("After removing last element: " +a);
+}
+}
