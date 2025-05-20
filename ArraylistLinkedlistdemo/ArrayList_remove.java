554626923c3039ea4db81cb0d09bb2d2fc8316f4
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 19:54:50 +0530

    Create ArrayList_remove.java

---
+++ ArraylistLinkedlistdemo/ArrayList_remove.java
@@ -0,0 +1,36 @@
+/*1b.Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+package adjlab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lab3 {
+    public static void main(String args[]) {
+        List<String> colors = new ArrayList<>();
+        
+        // Adding colors to the list
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Red");
+        colors.add("Navy");
+        colors.add("Brown");
+        colors.add("Maroon"); 
+        colors.add("Grey");   
+        colors.add("Pink");   
+
+        System.out.println("Initial list: " + colors);
+
+        
+        colors.remove("Green");
+        System.out.println("After removing Green: " + colors);
+
+        
+        if (colors.contains("Blue")) {
+            colors.remove("Blue");
+            System.out.println("After removing Blue: " + colors);
+        } else {
+            System.out.println("Blue not found");
+        }
+    }
+}
