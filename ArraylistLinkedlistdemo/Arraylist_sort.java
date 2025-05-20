171c1fb65a3c8e793086e615abff93e8f518e34d
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 19:57:40 +0530

    Create Arraylist_sort.java

---
+++ ArraylistLinkedlistdemo/Arraylist_sort.java
@@ -0,0 +1,26 @@
+/*Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+package adjlab;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class lab4 {
+	public static void main(String args[]){
+		List<String> color=new ArrayList<>();
+		color.add("Blue");
+		color.add("Green");
+		color.add("Red");
+		color.add("Navy");
+		color.add("Brown");
+		color.add("Maroon");
+		color.add("Grey");
+		System.out.println(color);
+		Collections.sort(color);
+		System.out.println(color);
+		
+
+}
+}
