007ffa5232e0933064f617ee92877c982ef393e4
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 20:09:33 +0530

    Create linkedlist_swap.java

---
+++ ArraylistLinkedlistdemo/linkedlist_swap.java
@@ -0,0 +1,27 @@
+/*Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+Collections.swap(l_list, 0, 2))*/
+package adjlab;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class lab11 {
+	public static void main(String [] args){
+        List<String> a = new ArrayList<>();
+        a.add("Blue");
+        a.add("Green");
+        a.add("Red");
+        a.add("Navy");
+        a.add("Brown");
+        a.add("Marroon");
+        a.add("Grey");
+        System.out.println("Original LinkedList:");
+        System.out.println(a);
+        Collections.swap(a,0,2);
+        System.out.println("\nLinkedList after swapping first and third elements:");
+        System.out.println(a);
+    }
+}
+
+
