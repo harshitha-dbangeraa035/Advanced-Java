78c5510bf4998c7da12d30e887566d95a02bc48e
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-20 20:08:04 +0530

    Create linkedlist_display.java

---
+++ ArraylistLinkedlistdemo/linkedlist_display.java
@@ -0,0 +1,25 @@
+/*Write a Java program to display elements and their positions in a linked list ( using
+l_listobj.get(p) )*/
+package adjlab;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class lab10 {
+	public static void main(String [] args){
+        List<String> a = new LinkedList<>();
+        a.add("Blue");
+        a.add("Green");
+        a.add("Red");
+        a.add("Navy");
+        a.add("Brown");
+        System.out.println(a);
+        System.out.println("Elements and their positions in the LinkedList:");
+        int n=a.size();
+        for(int i=0;i<n;i++){
+        System.out.println("Element at index "+i+" is: "+a.get(i)); 
+        } 
+    }
+
+}
