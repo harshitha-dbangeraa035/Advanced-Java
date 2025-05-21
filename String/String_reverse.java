bf19c5f9717dd3f0d4c5c0e20e529c1816a556d4
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-21 06:58:03 +0530

    Create String_reverse.java

---
+++ String/String_reverse.java
@@ -0,0 +1,27 @@
+/*Write a Java Program for Reversing the characters in a string using user defined function reverseString().*/
+package program3;
+import java.util.Scanner;
+
+public class lab4 {
+	 public static String reverseString(String str) {
+	        String reversed = "";
+	        for (int i =str.length()-1;i>=0;i--){
+	            reversed+=str.charAt(i);
+	        }
+	        return reversed;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc =new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String s = sc.nextLine();
+
+	        String result=reverseString(s);
+	        System.out.println(result);
+	    }
+	}
+
+
+
+
+    
