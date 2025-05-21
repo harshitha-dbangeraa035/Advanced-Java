6d986e7e5bd39c47ec029b3b7b0c0fc94480805a
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-21 14:12:03 +0530

    Create String_truncate.java

---
+++ String/String_truncate.java
@@ -0,0 +1,26 @@
+/*Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()*/
+package program3;
+
+import java.util.Scanner;
+
+public class lab8 {
+	 public static String truncate(String str, int length) {
+	        if (str.length()<=length) {
+	            return str;
+	        }
+	        return str.substring(0,length-3)+"..."; 
+	    }
+  public static void main(String[] args) {
+	        Scanner sc=new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String s=sc.nextLine();
+	        System.out.print("Enter max length: ");
+	        int len=sc.nextInt();
+	  
+	        String result = truncate(s,len);
+	        System.out.println(result);
+	    }
+	}
+
+
