af7f521f514f9901e140f76222f647fb1df654c0
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-21 13:59:57 +0530

    Create String_capitalize.java

---
+++ String/String_capitalize.java
@@ -0,0 +1,26 @@
+/*3c. Write a Java Program for Capitalizing the first letter of each word. using user defined function
+capitalizeWords()*/
+package program3;
+
+import java.util.Scanner;
+
+public class lab7 {
+
+    public static String capitalizeAfterSpace(String str) {
+        char[] chars=str.toCharArray();
+      for(int i=0;i<chars.length;i++) {
+            if(i==0||chars[i-1]==' ') {
+                chars[i]=Character.toUpperCase(chars[i]);
+            }
+        }
+
+        return new String(chars);
+    }
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s=sc.nextLine();
+        String result=capitalizeAfterSpace(s);
+        System.out.println(result);
+    }
+}
