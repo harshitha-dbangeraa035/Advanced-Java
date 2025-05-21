e2669740d35ce77ed29fbc68d972bc942b81e74e
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-21 14:23:14 +0530

    Create String_countWords.java

---
+++ String/String_countWords.java
@@ -0,0 +1,30 @@
+/*Write a Java Program for Counting the number of words in a string using user defined function
+countWords()*/
+package program3;
+
+import java.util.Scanner;
+
+public class lab11 {
+	 public static int countWords(String str) {
+	        int count=0;
+
+	        for (int i=0;i<str.length();i++) {
+	            if (str.charAt(i)!=' '&&(i==0||str.charAt(i - 1)==' ')) {
+	                count++;
+	            }
+	        }
+	        return count;
+	    }
+  public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String s = sc.nextLine();
+	        sc.close();
+          int result = countWords(s);
+	        System.out.println("Word count: " + result);
+	    }
+	}
+
+
+
+
