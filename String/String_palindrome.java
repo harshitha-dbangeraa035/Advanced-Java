02f3f352be878f347951e4aff558f76d4a01aad7
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-21 07:06:00 +0530

    Create String_palindrome.java

---
+++ String/String_palindrome.java
@@ -0,0 +1,31 @@
+/*Write a Java Program for Checking if a string reads the same backward as forward (ignoring case and punctuation) using user defined function isPalindrome():*/
+package program3;
+
+public class lab5 {
+	public static boolean isPalindrome(String s){
+		int left=0;
+		int right=s.length()-1;
+		while(left<right) {
+			if(s.charAt(left)==s.charAt(right)) {
+				return true;
+			}
+			left++;
+			right--;
+		}
+	return false;
+}
+
+public static void main(String[] args) {
+	String str="abs";
+	isPalindrome(str);
+	if(isPalindrome(str)) {
+		System.out.println("The string is a palindrome.");
+	}
+	else {
+		System.out.println("The string is not a palindrome.");
+	}
+
+}
+}
+
+
