4db28d0316fec216b1599fa682b56b200c0a124a
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-22 12:25:53 +0530

    Update Arraylist_sort.java

--- Program1_ArraylistLinkedlistdemo/Arraylist_sort.java
+++ Program1_ArraylistLinkedlistdemo/Arraylist_sort.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class lab4 {
+public class Arraylist_sort {
 	public static void main(String args[]){
 		List<String> color=new ArrayList<>();
 		color.add("Blue");
