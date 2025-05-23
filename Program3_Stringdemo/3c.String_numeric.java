9bfc58c84a5bce441c871b3e857d439469dd7a19
harshitha-dbangeraa035 <153203558+harshitha-dbangeraa035@users.noreply.github.com>
2025-05-23 19:02:17 +0530

    Update 3c.String_numeric.java

--- Program3_Stringdemo/3c.String_numeric.java
+++ Program3_Stringdemo/3c.String_numeric.java
@@ -1,4 +1,4 @@
-/*Write a Java Program for Verifying if a string contains only numeric characters using user defined
+/*3c.Write a Java Program for Verifying if a string contains only numeric characters using user defined
 function isNumeric()*/
 package program3;
 
